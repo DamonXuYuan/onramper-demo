afd5da74f7132a250a03afa65701d9d675e99142
Damon <330718757@qq.com>
2025-04-24 16:43:55 +0800

    done

---
+++ src/assets/js/onramperConfig.ts
@@ -0,0 +1,2 @@
+export const ONRAMPER_API_KEY = "pk_test_01JRH6YKK01Z6BHN74RCB7VWJD";
+export const ONRAMPER_WIDGET_URL = "https://widget-sandbox.onramper.com";
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { ONRAMPER_API_KEY } from "../../assets/js/onramperConfig";
+import styles from "./style.module.scss";
 
 const Home: React.FC = () => {
-  return <div>Init</div>;
+  const fiats = ["USD", "AUD"];
+  const cryptos = ["BTC", "ETH", "SOL"];
+
+  return (
+    <div className={styles.AppContainer}>
+      <iframe
+        src={`https://buy.onramper.dev?apiKey=${ONRAMPER_API_KEY}&mode=buy&onlyFiats=${fiats}&onlyCryptos=${cryptos}`}
+        title="Onramper Widget"
+        height="630px"
+        width="420px"
+        allow="accelerometer; autoplay; camera; gyroscope; payment; microphone"
+      />
+    </div>
+  );
 };
 
 export default Home;
